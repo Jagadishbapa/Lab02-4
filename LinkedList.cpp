788a1abbc31859d25751a7bf02e49ade65528293
k3n0bi <43155065+k3n0bi@users.noreply.github.com>
2018-09-25 10:27:36 -0600

    Working
    Got it working.

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -4,7 +4,6 @@
 // Definition of methods for the List class.
 
 #include "LinkedList.h"
-
 #include <utility>
 
 
@@ -37,6 +36,7 @@ const List & List::operator=(const List & other)
 		this -> ~List();
 		// and get a copy of other
 		first_ = clone(other.first_);
+
 	}
 
 	return *this;
@@ -76,7 +76,38 @@ void List::insertAsFirst(double x)
 	first_ = new Node(x, first_);
 }
 
+void List::insertAsLast(double x)
+{
+	
+	if (empty())
+	{
+    // object is empty, no need to re-invent the wheel.
+		insertAsFirst(x);
+	}
+	else
+	{
+		// object is not empty
+    Node * freshNode = new Node(x);  // create the new node
+    Node * ptr = first_->next_;
+    // Check to see if the next item in the linked list is a null pointer
+    // If it is, you're on the last node!
+    while (ptr->next_ != NULL)
+		{
+      ptr = ptr->next_; //iterate to next item in linked list
+		}
+    // Now we've arrived at the last node
+    // update the next_ from "null" to the location of our new node
+    ptr->next_ = freshNode;
+	}
+}
 
+/*while (ptr != NULL)
+	{
+		last->next_ = new Node(ptr->entry_);
+		last = last->next_;
+		ptr = ptr->next_;
+	}
+  */
 double List::removeFirst()
 {
 	double item = first_->entry_;
@@ -86,6 +117,24 @@ double List::removeFirst()
 	return item;
 }
 
+int List::size() const
+{
+	
+	int temp = 0;
+	if (!empty())
+	{
+		// The first entry is printed separately because no comma
+		// is needed.
+		temp++;
+		Node * ptr = first_->next_;
+		while (ptr != NULL)
+		{
+			temp++;
+			ptr = ptr->next_;
+		}
+	}
+	return temp;
+}
 
 void List::print(ostream & outfile) const
 {
@@ -103,9 +152,30 @@ void List::print(ostream & outfile) const
 		}
 	}
 	outfile << " ]";
+	outfile << endl;
+	outfile << "SUM: " << sum();
+	outfile << endl;
+	outfile << "SIZE: " << size();
+
 }
 
-
+double List::sum() const
+{
+	double total=0;
+	if (!empty())
+	{
+		// The first entry is printed separately because no comma
+		// is needed.
+		total = total + first_->entry_;
+		Node * ptr = first_->next_;
+		while (ptr != NULL)
+		{
+			total = total + ptr->entry_;
+			ptr = ptr->next_;
+		}
+	}
+	return total;
+}
 // Iterative version of clone.
 // This version walks down the linked structure making a
 //   new Node for each double in the structure.
